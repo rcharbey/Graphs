a2523d2db795075695f1fbf7d95c992a58c7afa8
rcharbey <r.charbey@gmail.com>
2018-05-28 16:38:34 +0200

    add compute val likers

---
+++ compute_val_likers.py
@@ -0,0 +1,50 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon May 28 16:24:34 2018
+
+@author: raphael
+"""
+
+from os import listdir
+from igraph import Graph
+
+import graph_utils 
+from numpy import mean
+
+DATA = '../Data/Graph_likers'
+
+tab_n = []
+tab_m = []
+tab_diam = []
+tab_densite = []
+tab_transit = []
+tab_centralisation = []
+tab_nb_com = []
+tab_mod = []
+
+for graph_name in listdir(DATA):
+    g = Graph.Read_GML('%s/%s' % (DATA, graph_name))
+    ci = ClassicIndicators(g)
+    
+    tab_n.append(len(g.vs))
+    tab_m.append(len(g.es))
+    tab_diam.append(ci.diameter())
+    tab_densite.append(ci.density())
+    tab_transit.append(ci.coeff_clustering())
+    tab_centralisation.append(ci.freeman_betweenness())
+    tab_nb_com.append(ci.nb_louvain_com())
+    tab_mod.append(ci.modularity()) 
+    
+def moyenne(t):
+    round(mean(t), 2)
+    
+print '%s : %s' % ('n', moyenne(tab_n))
+print '%s : %s' % ('m', moyenne(tab_m))
+print '%s : %s' % ('diametre', moyenne(tab_diam))
+print '%s : %s' % ('densite', moyenne(tab_densite))
+print '%s : %s' % ('transitivité', moyenne(tab_transit))
+print '%s : %s' % ('centralisation Freeman', moyenne(tab_centralisation))
+print '%s : %s' % ('nb communautés', moyenne(tab_nb_com))
+print '%s : %s' % ('modularité', moyenne(tab_mod))
+    
+    
